2b8f6855991a22f5f684016d4b1125ddcbd81e2d
Peter Lombardo <peterllombardo@gmail.com>
2023-07-23 08:04:47 -0400

    Changed lat long func

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from forms import SignUpForm, LoginForm, PreferenceForm
 from sqlalchemy.exc import IntegrityError
 from models import User, Weather, Tide, Preference, db, connect_db
 import requests
+import arrow
 
 CURR_USER_KEY = "curr_user"
 
@@ -85,11 +86,18 @@ def get_tidal_info(api_key, latitude, longitude):
     latitude = user.preference.latitude
     longitude = user.preference.longitude
     api_key = 'f7e98148-282d-11ee-86b2-0242ac130002-f7e981fc-282d-11ee-86b2-0242ac130002'
+    current_time = arrow.now()
+
+    start = current_time
+    end = current_time.shift(days=7)
+
+    start_date = start.format('YYYY-MM-DDTHH:mm:ss')
+    end_date = end.format('YYYY-MM-DDTHH:mm:ss')
 
 
     url = f'https://api.stormglass.io/v2/tide/extremes/point'
     headers = {'Authorization': api_key}
-    params = {'lat': latitude, 'lng': longitude}
+    params = {'lat': latitude, 'lng': longitude, 'start': start_date, 'end': end_date}
 
     response = requests.get(url, headers=headers, params=params)
 
@@ -99,6 +107,23 @@ def get_tidal_info(api_key, latitude, longitude):
         return data
     else:
         print(f'Error occurred during the API request. Status code: {response.status_code}')
+        return None
+
+def get_current_coords(location, api_key):
+    user = g.user
+    location = user.preference.location
+    api_key = '5b5d66b9a6ff4b5789d6d3a6ae9f7268'  
+    url = f'https://api.opencagedata.com/geocode/v1/json?q={location}&key={api_key}'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+         data = response.json()
+    if data.get('results'):
+            latitude = data['results'][0]['geometry']['lat']
+            longitude = data['results'][0]['geometry']['lng']
+            return latitude, longitude
+    
+    else:
         return None
 
 
@@ -171,28 +196,19 @@ def login():
 def set_up_prefs():
 
     form = PreferenceForm()
+
+    api_key = "5b5d66b9a6ff4b5789d6d3a6ae9f7268"
     
 
     if form.is_submitted() and form.validate():
-        location = form.location.data
-        temp_unit=form.temp_unit.data
-        air_temp=form.air_temp.data
-        tide_pref=form.tide_pref.data
-        time_of_day=form.time_of_day.data
+            location = form.location.data
+            temp_unit=form.temp_unit.data
+            air_temp=form.air_temp.data
+            tide_pref=form.tide_pref.data
+            time_of_day=form.time_of_day.data
         
-
-        
-        api_key = '5b5d66b9a6ff4b5789d6d3a6ae9f7268'  
-        url = f'https://api.opencagedata.com/geocode/v1/json?q={location}&key={api_key}'
-
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            if data.get('results'):
-                latitude = data['results'][0]['geometry']['lat']
-                longitude = data['results'][0]['geometry']['lng']
-                    
-                new_pref = Preference.create_preference(
+            latitude, longitude = get_current_coords(location, api_key)   
+            new_pref = Preference.create_preference(
                         user_id=g.user.id,
                         temp_unit=temp_unit,
                         air_temp=air_temp,
@@ -204,38 +220,52 @@ def set_up_prefs():
                     )
 
                     
-                db.session.commit()
-                return redirect('/')
+            db.session.commit()
+            return redirect('/')
 
-            else:
+    else:
                 # flash('Location not found. Please enter a valid location.', 'danger')
                 # flash('Error occurred during geocoding. Please try again later.', 'danger')
                 return render_template('initial_prefs.html', form=form)
             
-    return render_template('initial_prefs.html', form=form)
     
 @app.route('/user/edit', methods=['GET', 'POST'])
 def edit_user():
 
     user_id = g.user.id
     user = User.query.get(user_id)
+    api_key = "5b5d66b9a6ff4b5789d6d3a6ae9f7268"
 
     form = PreferenceForm(obj=user.preference)
 
+    
+
     if form.is_submitted() and form.validate():
 
-        preference = user.preference
-        preference.temp_unit = form.temp_unit.data
-        preference.air_temp = form.air_temp.data
-        preference.tide_pref = form.tide_pref.data
-        preference.time_of_day = form.time_of_day.data
-        preference.location = form.location.data
-       
-        db.session.commit()
-        return redirect('/')
-    else:
+        location = form.location.data
+        temp_unit = form.temp_unit.data
+        air_temp = form.air_temp.data
+        tide_pref = form.tide_pref.data
+        time_of_day = form.time_of_day.data
 
-        return render_template('edit.html', user=user, form=form)
+        latitude, longitude = get_current_coords(location, api_key)
+
+        if latitude is not None and longitude is not None:
+            preference = user.preference
+            preference.temp_unit = temp_unit
+            preference.air_temp = air_temp
+            preference.tide_pref = tide_pref
+            preference.time_of_day = time_of_day
+            preference.location = location
+            preference.latitude = latitude
+            preference.longitude = longitude
+
+            db.session.commit()
+            return redirect('/')
+
+        flash('Location not found. Please enter a valid location.', 'danger')
+
+    return render_template('edit.html', user=user, form=form)
 
 @app.route('/logout')
 def logout():
